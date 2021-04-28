63b482ea401584f855b147e0c26894fee8327a05
Gian Marco Paldino <gpaldino@ulb.ac.be>
2021-04-28 10:47:02 +0200

    Added JUnit Jupiter Maven dependecy, started testing Snapshot class

---
+++ src/test/java/library/SnapshotTest.java
@@ -0,0 +1,45 @@
+package library;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SnapshotTest {
+
+
+    @Test
+    public void TestEquals() {
+        int id = 1;
+        MockState1 state1 = new MockState1("bla",1);
+        MockState2 state2 = new MockState2('c',2.0);
+
+
+        Snapshot snapshot1 = new Snapshot(id,state1);
+        Snapshot snapshot2 = new Snapshot(id,state2);
+        Snapshot snapshot3 = new Snapshot(id+1,state2);
+
+        assertEquals(snapshot1,snapshot2);
+        assertNotEquals(snapshot2,snapshot3);
+
+    }
+}
+
+class MockState1 {
+    String randomString;
+    int randomInt;
+
+    public MockState1(String randomString, int randomInt) {
+        this.randomString = randomString;
+        this.randomInt = randomInt;
+    }
+}
+
+class MockState2 {
+    char randomChar;
+    double randomFloat;
+
+    public MockState2(char randomChar, double randomFloat) {
+        this.randomChar = randomChar;
+        this.randomFloat = randomFloat;
+    }
+}
