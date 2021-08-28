ec15f5b492d764ba6190173421d338f296114bad
Luca Danelutti <lucadanelutti@outlook.com>
2021-08-28 11:11:36 +0200

    Simple refactorization

--- functional_testing/oilWellsAutomated.py
+++ functional_testing/oilWellsAutomated.py
@@ -1,28 +1,32 @@
-import subprocess
+import subprocess, time
 from subprocess import PIPE, STDOUT
 
 from env import *
 
-app1 = subprocess.Popen([java_path, r'-jar', jar_path], stdout=PIPE, stdin=PIPE, stderr=STDOUT, bufsize=0)
-app2 = subprocess.Popen([java_path, r'-jar', jar_path], stdout=PIPE, stdin=PIPE, stderr=STDOUT, bufsize=0)
-app3 = subprocess.Popen([java_path, r'-jar', jar_path], stdout=PIPE, stdin=PIPE, stderr=STDOUT, bufsize=0)
+oilAmount = 1000
 
-app1.stdin.write(b"initialize, localhost, 11111, 1000\n")
-app2.stdin.write(b"initialize, localhost, 11112, 2000\n")
-app3.stdin.write(b"initialize, localhost, 11113, 3000\n")
-app2.stdin.write(b"join, localhost, 11111\n")
-app3.stdin.write(b"join, localhost, 11111\n")
+with open('outfile', 'w') as outfile_w:
+    app1 = subprocess.Popen([java_path, r'-jar', jar_path, "1"], stdout=outfile_w, stdin=PIPE, stderr=subprocess.STDOUT, bufsize=0)
+    app2 = subprocess.Popen([java_path, r'-jar', jar_path, "2"], stdout=outfile_w, stdin=PIPE, stderr=subprocess.STDOUT, bufsize=0)
+    app3 = subprocess.Popen([java_path, r'-jar', jar_path, "3"], stdout=outfile_w, stdin=PIPE, stderr=subprocess.STDOUT, bufsize=0)
 
-# here add the commands that you wish to execute
+app1.stdin.write(bytes("initialize, localhost, 10000, " + str(oilAmount) + "\n", 'utf-8'))
+app2.stdin.write(bytes("initialize, localhost, 10001, " + str(oilAmount) + "\n", 'utf-8'))
+app3.stdin.write(bytes("initialize, localhost, 10002, " + str(oilAmount) + "\n", 'utf-8'))
+app2.stdin.write(b"join, localhost, 10000\n")
+app3.stdin.write(b"join, localhost, 10000\n")
 
 app1.stdin.flush()
 app2.stdin.flush()
 app3.stdin.flush()
-while True:
-    print("[APP1] " + app1.stdout.readline().decode())
-    print("[APP2] " + app2.stdout.readline().decode())
-    print("[APP3] " + app3.stdout.readline().decode())
 
-# TODO: there should be a better way to read lines simultaneously from all the application stdin
+with open('outfile', 'r') as outfile_r:
+    while True:
+        line = outfile_r.readline()
+        if not line:
+            time.sleep(1)
+        else:
+            print(line, end="")
+
 # useful resource
 # https://eli.thegreenplace.net/2017/interacting-with-a-long-running-child-process-in-python/
