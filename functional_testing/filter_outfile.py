38556e37d6fec5ba5cae660a7a4a536eb4c897a7
ian-ofgod <ian.didio@mail.polimi.it>
2021-09-07 11:09:10 +0200

    makes outfile more readable

--- functional_testing/filter_outfile.py
+++ functional_testing/filter_outfile.py
@@ -1,11 +1,14 @@
-outfile_r=open('outfile', 'r')
-filtered_r=open('filtered_outfile', 'w')
+import re
+
+outfile_r = open('outfile.txt', 'r')
+filtered_r = open('filtered_outfile.txt', 'w')
 
 lines = outfile_r.readlines()
 
 for line in lines:
-    if line.startswith("APP"):
-        filtered_r.write(line)
+    new_line = re.sub(r'(oilwells)(.*?)(-)', '-', line)
+    new_line = re.sub(r'(\${main:0})', '0', new_line)
+    filtered_r.write(new_line)
 
 outfile_r.close()
 filtered_r.close()
